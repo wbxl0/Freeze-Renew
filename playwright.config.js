427dc0a2877746d1300b4b966b790c66a3edd450
losy-mify <louisemiffyi@gmail.com>
2026-03-14 13:29:58 +0800

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
+  testMatch: '**/*.spec.js',
   timeout: 60000,
   use: {
     headless: true,
